6a725477c711777607261a5f8f96934c97282429
guilhermezica <58021144+guilhermezica@users.noreply.github.com>
2024-08-06 18:13:08 +1000

    Add files via upload
    First stage of the Account project

---
+++ Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Account
+    {
+        //Instance variables
+        String name;
+        decimal balance;
+
+        // Accessor Methods
+        public Account(String name, decimal balance)
+        {
+            this.balance = balance;
+            this.name = name;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            this.balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            this.balance -= amount;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.name + "'s balance is: $" + this.balance + " dollars");
+        }
+
+        public String Name()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ TestAccount.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankAccount;
+
+Account BillZica = new Account("Bill Zica", 12.23m);
+
+BillZica.Print();
+
+BillZica.Deposit(5.13m);
+BillZica.Print();
+
+BillZica.Withdraw(-25.12m);
+BillZica.Print();
+
+//Realized if I withdraw a negative amount,
+//it's the same as a deposit, so I need to
+//include data validation for this input
+
+BillZica.Withdraw(81.50m);
+BillZica.Print();
+
+//Account balance goes into negative
+//territory which is good for now.
